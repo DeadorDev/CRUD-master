15e3f44858916d1414cd4a4e23bd1db9c15d5394
Deador <inkohnito4@gmail.com>
2021-12-17 15:14:13 +0200

    Update 0.3

--- src/main/java/deador/springmvc/controllers/FirstController.java
+++ src/main/java/deador/springmvc/controllers/FirstController.java
@@ -1,20 +1,53 @@
 package deador.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "number_1", required = false) int number_1,
+                             @RequestParam(value = "number_2", required = false) int number_2,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        double result;
+        switch (action) {
+            case "multiplication":
+                result = number_1 * number_2;
+                break;
+            case "division":
+                result = number_1 / (double) number_2;
+                break;
+            case "subtraction":
+                result = number_1 - number_2;
+                break;
+            case "addition":
+                result = number_1 + number_2;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("messageFromCalculator", result);
+        return "first/calculator";
     }
 }
