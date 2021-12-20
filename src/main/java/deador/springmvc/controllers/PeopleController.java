e32e7d568ac60387cc94accd212594512d7b50f1
Deador <inkohnito4@gmail.com>
2021-12-20 17:58:24 +0200

    Update 0.2 in CRUD

---
+++ src/main/java/deador/springmvc/controllers/PeopleController.java
@@ -0,0 +1,84 @@
+package deador.springmvc.controllers;
+
+import deador.springmvc.dao.PersonDAO;
+import deador.springmvc.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    //----------------------------------------------------------------
+    //Get default page with all people
+    @GetMapping()
+    public String index(Model model) {
+        // Получим всех людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    //Get a page with one person per id
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Получим одного человека по id из DAO и передадим на отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    //----------------------------------------------------------------
+    //Get a creation page
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    //Save a person
+    @PostMapping
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    //----------------------------------------------------------------
+    //Get a edit page
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    //Update a person
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    //----------------------------------------------------------------
+    //Delete a person
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+}
